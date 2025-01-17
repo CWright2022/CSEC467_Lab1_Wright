7fcfda2dd114078254e7f5d0741891cb5fab0e31
Cayden Wright <cwright2022@gmail.com>
2025-01-16 22:26:53 -0500

    meets lab requirements

--- app/src/main/java/com/csec467/myapplication/SecondActivity.java
+++ app/src/main/java/com/csec467/myapplication/SecondActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,27 +13,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        String userText = intent.getStringExtra("userText");
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button mainButton = (Button)findViewById(R.id.launchSecondBtn);
-        EditText textBox = (EditText)findViewById(R.id.editabletextBox);
-        mainButton.setOnClickListener(new View.OnClickListener(){
+        Button toastButton = (Button)findViewById(R.id.button);
+        toastButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("userText", textBox.getText().toString());
-                startActivity(intent);
+                Toast toast = Toast.makeText(getBaseContext(), userText, Toast.LENGTH_LONG);
+                toast.show();
             }
         });
     }
